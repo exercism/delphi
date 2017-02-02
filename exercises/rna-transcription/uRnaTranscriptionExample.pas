57700a87cf1a16e544b07acfba2fadf6ea17d337
Ryan Potts <rpottsoh@gmail.com>
2017-02-02 16:02:16 -0500

    rna-transcription: example solution
    This solution satifies all test cases laid out in canonical data.

--- exercises/rna-transcription/uRnaTranscriptionExample.pas
+++ exercises/rna-transcription/uRnaTranscriptionExample.pas
@@ -1,14 +1,55 @@
 unit uRnaTranscription;
 
 interface
+uses Generics.Collections;
 
-  function Hello(name: string='World'): string;
+type
+  complement = class
+  private
+    class var
+      fDnaToRna: TDictionary<char, char>;
+  public
+    class function OfDna(nucleotide: string): string; static;
+  end;
 
 implementation
+uses SysUtils;
 
-function Hello(name: string='World'): string;
+class function complement.OfDna(nucleotide: string): string;
+var i: integer;
+    lRNA: string;
+    elements: TArray<char>;
+    RNAelement: char;
 begin
-  result := 'Hello, ' + name + '!';
+  result := '';
+  lRNA := '';
+  if not trim(nucleotide).IsEmpty then
+  begin
+    fDnaToRna := TDictionary<char, char>.Create;
+    with fDnatoRna do
+    begin
+      add('G','C');
+      add('C','G');
+      add('T','A');
+      add('A','U');
+    end;
+    elements := nucleotide.ToUpper.ToCharArray;
+    i := 0;
+    while i <= high(elements) do
+    begin
+      if fDnaToRna.TryGetValue(elements[i],RNAelement) then
+      begin
+        lRNA := lRNA + RNAelement;
+        inc(i);
+      end
+      else
+      begin
+        lRNA := '';
+        break;
+      end;
+    end;
+  end;
+  result := lRNA;
 end;
 
 end.
