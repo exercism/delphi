bb81612b5c3dc5074ee64289f2e75e4985f7957e
Ryan Potts <rpottsoh@gmail.com>
2017-12-03 21:30:14 -0500

    bob: update to v1.1.0
    Updated test suite and example solution to conform to v1.1.0 of test data.

--- exercises/bob/uBobExample.pas
+++ exercises/bob/uBobExample.pas
@@ -8,8 +8,9 @@ type
     class function IsSilence(aStatement: string): Boolean; static;
     class function IsYelling(aStatement: string): Boolean; static;
     class function IsQuestion(aStatement: string): Boolean; static;
+    class function IsYellingQuestion(aStatement: string): Boolean; static;
   public
-    class function Hey(aStatement: string): string; static;
+    class function Response(aStatement: string): string; static;
   End;
 
 implementation
@@ -25,16 +26,24 @@ begin
   result := (aStatement.ToUpper = aStatement) and TRegEx.IsMatch(aStatement, '[a-zA-Z]+');
 end;
 
+class function TBob.IsYellingQuestion(aStatement: string): Boolean;
+begin
+  result := IsQuestion(aStatement) and IsYelling(aStatement);
+end;
+
 class function TBob.IsQuestion(aStatement: string): Boolean;
 begin
   result := aStatement.Trim.EndsWith('?');
 end;
 
-class function TBob.Hey(aStatement: string): string;
+class function TBob.Response(aStatement: string): string;
 begin
   if IsSilence(aStatement) then
-    result := 'Fine.  Be that way!'
+    result := 'Fine. Be that way!'
   else
+    if IsYellingQuestion(aStatement) then
+      result := 'Calm down, I know what I''m doing!'
+    else
     if IsYelling(aStatement) then
       result := 'Whoa, chill out!'
     else
