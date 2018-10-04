1e8bed9be0954455f6ea922875e649a93f1b84b0
Filip Toskovic <filip.toskovic@lastbajt.com>
2018-10-04 05:50:52 +0200

    scrabble-score: add new exercise

---
+++ exercises/scrabble-score/uScrabbleScoreExample.pas
@@ -0,0 +1,40 @@
+unit uScrabbleScore;
+
+interface
+
+uses
+  System.Generics.Collections;
+
+type
+  TScrabble = class
+  private
+    type
+      TC = array ['A' .. 'Z'] of integer;
+    class var FValues : TC;
+    class constructor Create;
+  public
+    class function Score(AInp : string) : integer;
+  end;
+
+implementation
+
+uses
+  System.SysUtils;
+
+class constructor TScrabble.Create;
+const
+  LValues : TC = (1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10);
+begin
+  FValues := LValues;
+end;
+
+class function TScrabble.Score(AInp: string): integer;
+var
+  C: char;
+begin
+  Result := 0;
+  for C in AInp.ToUpperInvariant do
+    Result := Result + FValues[C];
+end;
+
+end.
