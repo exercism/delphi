546e7d2dc125582c7bafb8353af1ff836752dcf9
Ryan Potts <rpottsoh@gmail.com>
2018-10-10 13:29:10 -0400

    correct spelling of a variable

--- exercises/gigasecond/uGigasecondExample.pas
+++ exercises/gigasecond/uGigasecondExample.pas
@@ -7,18 +7,18 @@ const
 
 type
   TGigasecond = class
-    class function Add(ABithDate : string) : string;
+    class function Add(ABirthDate : string) : string;
   end;
 
 implementation
 
 uses System.DateUtils, System.SysUtils;
 
-class function TGigasecond.Add(ABithDate: string): string;
+class function TGigasecond.Add(ABirthDate: string): string;
 const
   GigaSecInDays = 1000000000 / SecsPerDay;
 begin
-  Result := DateToISO8601(ISO8601ToDate(ABithDate) + GigaSecInDays).Remove(19);
+  Result := DateToISO8601(ISO8601ToDate(ABirthDate) + GigaSecInDays).Remove(19);
 end;
 
 end.
