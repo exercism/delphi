3a9f69608530acd85399fedcb7cd7fb1b8be2176
Filip Toskovic <filip.toskovic@lastbajt.com>
2018-10-13 17:43:13 +0200

    robot-name: reset test update

--- exercises/robot-name/uRobotNameTest.pas
+++ exercises/robot-name/uRobotNameTest.pas
@@ -2,7 +2,7 @@ unit uRobotNameTest;
 
 interface
 uses
-  DUnitX.TestFramework, uRobotName;
+  DUnitX.TestFramework, uRobotName, System.Generics.Collections;
 
 const
   CanonicalVersion = '0.0.0';
@@ -12,7 +12,14 @@ type
   TRobotNameTest = class(TObject)
   private
     FRobot : TRobot;
+    Names : TList<string>;
+    Robots : TObjectList<TRobot>;
   public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
     [Test]
 //    [Ignore('Comment the "[Ignore]" statement to run the test')]
     procedure name_properly_formated;
@@ -33,7 +40,7 @@ type
 implementation
 
 uses
-  System.RegularExpressions, System.Classes, Generics.Collections, SysUtils;
+  System.RegularExpressions, System.Classes, SysUtils;
 
 procedure TRobotNameTest.is_name_persistent;
 begin
@@ -49,37 +56,57 @@ begin
   FRobot.DisposeOf;
 end;
 
-procedure TRobotNameTest.each_robot_have_unique_name;
-var Names : TList<string>;
-  Robots : TObjectList<TRobot>;
-  i, j: char;
+procedure TRobotNameTest.Setup;
+var i, j : char;
   k : integer;
 begin
   Names := TList<string>.create;
-  Robots := TObjectList<TRobot>.Create;
   for i := 'A' to 'Z' do
     for j := 'A' to 'Z' do
       for k := 0 to 999 do
         Names.Add(i + j + format('%.*d', [3, k]));
-  for k := 1 to 1000 do
-  begin
-    Robots.Add(TRobot.Create);
-    Assert.IsTrue(Names.Contains(Robots.Last.Name), Robots.Last.Name);
-    Names.Remove(Robots.Last.Name);
-  end;
+  Robots := TObjectList<TRobot>.Create;
+end;
+
+procedure TRobotNameTest.TearDown;
+begin
   Names.DisposeOf;
   Robots.DisposeOf;
 end;
 
+procedure TRobotNameTest.each_robot_have_unique_name;
+var
+  k : integer;
+begin
+  for k := 1 to 1000 do
+  begin
+    Robots.Add(TRobot.Create);
+    Assert.IsTrue(Names.Contains(Robots.Last.Name), 'Robot name is not unique');
+    Names.Remove(Robots.Last.Name);
+  end;
+end;
+
 procedure TRobotNameTest.is_able_to_reset_name;
 var
-  Old: string;
+  old : string;
+  i, changeCount : Integer;
+
 begin
-  FRobot := TRobot.Create;
-  Old := FRobot.Name;
-  FRobot.Reset;
-  Assert.IsTrue(TRegEx.IsMatch(FRobot.Name, '^[A-Z]{2}\d{3}$'), 'expected format''ssddd'', found ' + FRobot.Name);
-  FRobot.DisposeOf;
+  Robots.Add(TRobot.Create);
+  old := Robots.Last.Name;
+  Names.Remove(old);
+  changeCount := 0;
+  for i := 0 to 10 do
+  begin
+    Names.Add(old);
+    old := Robots.Last.Name;
+    Robots.Last.Reset;
+    Assert.IsTrue(Names.Contains(Robots.Last.Name), 'Robot name after reset is not unique');
+    Names.Remove(Robots.Last.Name);
+    if old <> Robots.Last.Name then
+      inc(changeCount);
+  end;
+  Assert.AreNotEqual(0, changeCount, 'Robot name after reset is not changed');
 end;
 
 initialization
