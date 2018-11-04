70e9588d4f8689c23292f881acab073d837284fa
Filip Toskovic <filip.toskovic@lastbajt.com>
2018-11-04 01:19:14 +0100

    high-scores: add new exercise (#325)

---
+++ exercises/high-scores/uHighScoresExample.pas
@@ -0,0 +1,93 @@
+unit uHighScores;
+
+interface
+
+type
+  IScores = interface
+    function Input(AInput : TArray<integer>) : ISCores;
+    function Scores :  TArray<integer>;
+    function Latest : integer;
+    function Highest : integer;
+    function Top : TArray<integer>;
+    function Report : string;
+  end;
+
+function NewScores: IScores;
+
+implementation
+
+uses
+  System.Generics.Collections, System.SysUtils;
+
+type
+
+  TScores = class(TInterfacedObject, IScores)
+  private
+    FScores : TArray<integer>;
+  public
+    function Input(AInput : TArray<integer>) : ISCores;
+    function Scores :  TArray<integer>;
+    function Latest : integer;
+    function Highest : integer;
+    function Top : TArray<integer>;
+    function Report : string;
+  end;
+
+ { TScores }
+
+function NewScores: IScores;
+begin
+  result := TScores.Create;
+end;
+
+function TScores.Highest: integer;
+var l : TList<integer>;
+begin
+  l := TList<integer>.Create;
+  l.AddRange(FScores);
+  l.Sort;
+  Result := l.Last;
+  l.DisposeOf;
+end;
+
+function TScores.Input(AInput: TArray<integer>): ISCores;
+begin
+  FScores := AInput;
+  Result := self;
+end;
+
+function TScores.Latest: integer;
+begin
+  Result := FScores[High(FScores)];
+end;
+
+function TScores.Report: string;
+var L, H  : integer;
+begin
+  L := Latest;
+  H := Highest;
+  if L = H then
+    Result := format('Your latest score was %d. That''s your personal best!',[L])
+  else
+    Result := format('Your latest score was %d. That''s %d short of your personal best!',[L, H - L]);
+end;
+
+function TScores.Scores: TArray<integer>;
+begin
+  Result := FScores;
+end;
+
+function TScores.Top: TArray<integer>;
+var l : TList<integer>;
+begin
+  l := TList<integer>.Create;
+  l.AddRange(FScores);
+  l.Sort;
+  l.Reverse;
+  if l.Count > 3 then
+    l.Count := 3;
+  Result := l.ToArray;
+  l.DisposeOf;
+end;
+
+end.
