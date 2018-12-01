35bb1bd28cde6e30878b8eac152966fadd177f78
Ryan Potts <rpottsoh@gmail.com>
2018-12-01 12:26:05 -0500

    Hamming: Update to v2.2.0
    per exercism/problem-specifications#1412

--- exercises/hamming/uHammingTests.pas
+++ exercises/hamming/uHammingTests.pas
@@ -5,10 +5,9 @@ uses
   DUnitX.TestFramework;
 
 const
-  CanonicalVersion = '2.1.0';
+  CanonicalVersion = '2.2.0';
 
 type
-
   [TestFixture]
   HammingTests = class(TObject)
   public
@@ -18,7 +17,11 @@ type
 
     [Test]
     [Ignore]
-    procedure identical_strands;
+    procedure single_letter_identical_strands;
+
+    [Test]
+    [Ignore]
+    procedure single_letter_different_strands;
 
     [Test]
     [Ignore]
@@ -26,43 +29,7 @@ type
 
     [Test]
     [Ignore]
-    procedure complete_distance_in_single_nucleotide_strands;
-
-    [Test]
-    [Ignore]
-    procedure complete_distance_in_small_strands;
-
-    [Test]
-    [Ignore]
-    procedure small_distance_in_small_strands;
-
-    [Test]
-    [Ignore]
-    procedure small_distance;
-
-    [Test]
-    [Ignore]
-    procedure small_distance_in_long_strands;
-
-    [Test]
-    [Ignore]
-    procedure non_unique_character_in_first_strand;
-
-    [Test]
-    [Ignore]
-    procedure non_unique_character_in_second_strand;
-
-    [Test]
-    [Ignore]
-    procedure same_nucleotides_in_different_positions;
-
-    [Test]
-    [Ignore]
-    procedure large_distance;
-
-    [Test]
-    [Ignore]
-    procedure large_distance_in_off_by_one_strand;
+    procedure long_different_strands;
 
     [Test]
     [Ignore]
@@ -81,62 +48,22 @@ begin
   Assert.AreEqual(0,THamming.Distance('',''));
 end;
 
-procedure HammingTests.identical_strands;
+procedure HammingTests.single_letter_identical_strands;
 begin
   Assert.AreEqual(0,THamming.Distance('A','A'));
 end;
 
+procedure HammingTests.single_letter_different_strands;
+begin
+  Assert.AreEqual(1, THamming.Distance('G', 'T'));
+end;
+
 procedure HammingTests.long_identical_strands;
 begin
-  Assert.AreEqual(0, THamming.Distance('GGACTGA', 'GGACTGA'));
+  Assert.AreEqual( 0, THamming.Distance('GGACTGAAATCTG', 'GGACTGAAATCTG'));
 end;
 
-procedure HammingTests.complete_distance_in_single_nucleotide_strands;
-begin
-  Assert.AreEqual(1, THamming.Distance('A', 'G'));
-end;
-
-procedure HammingTests.complete_distance_in_small_strands;
-begin
-  Assert.AreEqual( 2, THamming.Distance('AG', 'CT'));
-end;
-
-procedure HammingTests.small_distance_in_small_strands;
-begin
-  Assert.AreEqual(1, THamming.Distance('AT', 'CT'));
-end;
-
-procedure HammingTests.small_distance;
-begin
-  Assert.AreEqual(1, THamming.Distance('GGACG', 'GGTCG'));
-end;
-
-procedure HammingTests.small_distance_in_long_strands;
-begin
-  Assert.AreEqual(2, THamming.Distance('ACCAGGG', 'ACTATGG'));
-end;
-
-procedure HammingTests.non_unique_character_in_first_strand;
-begin
-  Assert.AreEqual(1, THamming.Distance('AAG', 'AAA'));
-end;
-
-procedure HammingTests.non_unique_character_in_second_strand;
-begin
-  Assert.AreEqual(1, THamming.Distance('AAA', 'AAG'));
-end;
-
-procedure HammingTests.same_nucleotides_in_different_positions;
-begin
-  Assert.AreEqual(2, THamming.Distance('TAG', 'GAT'));
-end;
-
-procedure HammingTests.large_distance;
-begin
-  Assert.AreEqual(4, THamming.Distance('GATACA', 'GCATAA'));
-end;
-
-procedure HammingTests.large_distance_in_off_by_one_strand;
+procedure HammingTests.long_different_strands;
 begin
   Assert.AreEqual(9, THamming.Distance('GGACGGATTCTG', 'AGGACGGATTCT'));
 end;
