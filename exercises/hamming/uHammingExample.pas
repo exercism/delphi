13585c1cb98fdbc3ca5ea35c6ef9b54fc360a66b
Ryan Potts <rpottsoh@gmail.com>
2019-02-07 20:53:16 -0500

    Hamming: update to v2.3.0
    per exercism/problem-specifications#1450

--- exercises/hamming/uHammingExample.pas
+++ exercises/hamming/uHammingExample.pas
@@ -12,12 +12,18 @@ implementation
 uses System.SysUtils;
 
 class function THamming.Distance(strand1, strand2: string): integer;
-var i: integer;
 begin
+  if strand1.IsEmpty and not strand2.IsEmpty then
+    raise EArgumentException.Create('error: left strand must not be empty');
+
+  if not strand1.IsEmpty and strand2.IsEmpty then
+    raise EArgumentException.Create('error: right strand must not be empty');
+
   if strand1.Length <> strand2.Length  then
     raise EArgumentException.Create('error: left and right strands must be of equal length');
+
   result := 0;
-  for i := Low(strand1) to High(strand1) do
+  for var i := Low(strand1) to High(strand1) do
     result := result + ord(strand1[i] <> strand2[i]);
 end;
 
