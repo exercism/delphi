4b92df80afc1a87dec139ffa0d8c3e565a18615c
Ryan Potts <rpottsoh@gmail.com>
2019-03-26 21:00:07 -0400

    Triangle: update to v1.2.1
    per exercism/problem-specifications#1483

--- exercises/triangle/uTestTriangle.pas
+++ exercises/triangle/uTestTriangle.pas
@@ -4,91 +4,91 @@ interface
 uses 
   DUnitX.TestFramework; 
   
-const CanonicalVersion = '1.2.0';
+const CanonicalVersion = '1.2.1';
 
 type
-  [TestFixture('Equilateral')] 
-  EquilateralTests = class(TObject) 
+  [TestFixture('Equilateral Triangle')]
+  EquilateralTests = class(TObject)
   public
     [Test]
 //    [Ignore('Comment the "[Ignore]" statement to run the test')]
-    procedure True_if_all_sides_are_equal;
+    procedure AllSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure False_if_any_side_is_unequal;
+    procedure AnySideIsUnequal;
 
     [Test]
     [Ignore]
-    procedure False_if_no_sides_are_equal;
+    procedure NoSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure All_zero_sides_are_illegal_so_the_triangle_is_not_equilateral;
+    procedure AllZeroSidesIsNotATriangle;
 
     [Test]
     [Ignore]
     procedure Sides_may_be_floats;
   end;
 
-  [TestFixture('Isosceles')]
+  [TestFixture('Isosceles Triangle')]
   IsoscelesTests = class(TObject)
   public
     [Test]
     [Ignore]
-    procedure True_if_last_two_sides_are_equal;
+    procedure LastTwoSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure True_if_first_two_sides_are_equal;
+    procedure FirstTwoSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure True_if_first_and_last_sides_are_equal;
+    procedure FirstAndLastSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure Equilateral_triangles_are_also_isosceles;
+    procedure EquilateralTrianglesAreAlsoIsosceles;
 
     [Test]
     [Ignore]
-    procedure False_if_no_sides_are_equal;
+    procedure NoSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure Sides_that_violate_triangle_inequality_are_not_isosceles_even_if_two_are_equal_1;
+    procedure FirstTriangleInequalityViolation;
 
     [Test]
     [Ignore]
-    procedure Sides_that_violate_triangle_inequality_are_not_isosceles_even_if_two_are_equal_2;
+    procedure SecondTriangleInequalityViolation;
 
     [Test]
     [Ignore]
-    procedure Sides_that_violate_triangle_inequality_are_not_isosceles_even_if_two_are_equal_3;
+    procedure ThirdTriangleInequalityViolation;
 
     [Test]
     [Ignore]
     procedure Sides_may_be_floats;
   end;
 
-  [TestFixture('Scalene')]
+  [TestFixture('Scalene Triangle')]
   ScaleneTests = class(TObject)
   public
     [Test]
     [Ignore]
-    procedure True_if_no_sides_are_equal;
+    procedure NoSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure False_if_all_sides_are_equal;
+    procedure AllSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure False_if_two_sides_are_equal;
+    procedure TwoSidesAreEqual;
 
     [Test]
     [Ignore]
-    procedure Sides_that_violate_triangle_inequality_are_not_scalene_even_if_they_are_all_different;
+    procedure MayNotViolateTriangleInequality;
 
     [Test]
     [Ignore]
@@ -99,23 +99,23 @@ implementation
 uses uTriangle; 
  
 {$region 'EquilateralTests'} 
-procedure EquilateralTests.True_if_all_sides_are_equal; 
+procedure EquilateralTests.AllSidesAreEqual;
 begin 
   Assert.AreEqual(true, Triangle.Sides(Equilateral, 2, 2, 2)); 
 end; 
  
-procedure EquilateralTests.False_if_any_side_is_unequal; 
+procedure EquilateralTests.AnySideIsUnequal;
 begin 
   Assert.AreEqual(false, Triangle.Sides(Equilateral, 2, 3, 2)); 
 end; 
  
-procedure EquilateralTests.False_if_no_sides_are_equal; 
-begin 
-  Assert.AreEqual(false, Triangle.Sides(Equilateral, 5, 4, 6)); 
-end; 
- 
-procedure EquilateralTests.All_zero_sides_are_illegal_so_the_triangle_is_not_equilateral; 
-begin 
+procedure EquilateralTests.NoSidesAreEqual;
+begin
+  Assert.AreEqual(false, Triangle.Sides(Equilateral, 5, 4, 6));
+end;
+
+procedure EquilateralTests.AllZeroSidesIsNotATriangle;
+begin
   Assert.AreEqual(false, Triangle.Sides(Equilateral, 0, 0, 0)); 
 end; 
  
@@ -124,44 +124,44 @@ begin
   Assert.AreEqual(true, Triangle.Sides(Equilateral, 0.5, 0.5, 0.5)); 
 end; 
 {$endregion} 
- 
+
 {$region'IsoscelesTests'} 
-procedure IsoscelesTests.True_if_last_two_sides_are_equal; 
+procedure IsoscelesTests.LastTwoSidesAreEqual;
 begin 
   Assert.AreEqual(true, Triangle.Sides(Isosceles, 3, 4, 4)); 
 end;
 
-procedure IsoscelesTests.True_if_first_two_sides_are_equal;
+procedure IsoscelesTests.FirstTwoSidesAreEqual;
 begin
   Assert.AreEqual(true, Triangle.Sides(Isosceles, 4, 4, 3));
 end;
 
-procedure IsoscelesTests.True_if_first_and_last_sides_are_equal;
+procedure IsoscelesTests.FirstAndLastSidesAreEqual;
 begin
   Assert.AreEqual(true, Triangle.Sides(Isosceles, 4, 3, 4));
 end;
 
-procedure IsoscelesTests.Equilateral_triangles_are_also_isosceles;
+procedure IsoscelesTests.EquilateralTrianglesAreAlsoIsosceles;
 begin
   Assert.AreEqual(true, Triangle.Sides(Isosceles, 4, 4, 4));
 end;
 
-procedure IsoscelesTests.False_if_no_sides_are_equal;
+procedure IsoscelesTests.NoSidesAreEqual;
 begin
   Assert.AreEqual(false, Triangle.Sides(Isosceles, 2, 3, 4));
 end;
 
-procedure IsoscelesTests.Sides_that_violate_triangle_inequality_are_not_isosceles_even_if_two_are_equal_1;
+procedure IsoscelesTests.FirstTriangleInequalityViolation;
 begin
   Assert.AreEqual(false, Triangle.Sides(Isosceles, 1, 1, 3));
 end;
 
-procedure IsoscelesTests.Sides_that_violate_triangle_inequality_are_not_isosceles_even_if_two_are_equal_2;
+procedure IsoscelesTests.SecondTriangleInequalityViolation;
 begin
   Assert.AreEqual(false, Triangle.Sides(Isosceles, 1, 3, 1));
 end;
 
-procedure IsoscelesTests.Sides_that_violate_triangle_inequality_are_not_isosceles_even_if_two_are_equal_3;
+procedure IsoscelesTests.ThirdTriangleInequalityViolation;
 begin
   Assert.AreEqual(false, Triangle.Sides(Isosceles, 3, 1, 1));
 end;
@@ -173,22 +173,22 @@ end;
 {$endregion} 
  
 {$region 'ScaleneTests'} 
-procedure ScaleneTests.True_if_no_sides_are_equal; 
+procedure ScaleneTests.NoSidesAreEqual;
 begin 
   Assert.AreEqual(true, Triangle.Sides(Scalene, 5, 4, 6)); 
 end; 
  
-procedure ScaleneTests.False_if_all_sides_are_equal; 
+procedure ScaleneTests.AllSidesAreEqual;
 begin 
   Assert.AreEqual(false, Triangle.Sides(Scalene, 4, 4, 4)); 
 end; 
  
-procedure ScaleneTests.False_if_two_sides_are_equal; 
+procedure ScaleneTests.TwoSidesAreEqual;
 begin 
   Assert.AreEqual(false, Triangle.Sides(Scalene, 4, 4, 3)); 
 end; 
  
-procedure ScaleneTests.Sides_that_violate_triangle_inequality_are_not_scalene_even_if_they_are_all_different; 
+procedure ScaleneTests.MayNotViolateTriangleInequality;
 begin 
   Assert.AreEqual(false, Triangle.Sides(Scalene, 7, 3, 2)); 
 end; 
